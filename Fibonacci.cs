4bf03779c0a3a1dee0f9f70071d527d531b84f5f
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-05-25 09:54:37 +0900

    초기 커밋.

---
+++ Fibonacci.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionPrac
+{
+    internal class Fibonacci
+    {
+        public long Get(int n)
+        {
+            if (n < 0) { return 0; }
+            if (n == 1) { return 1; }
+            return Get(n - 2) * Get(n - 1);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fibo = new Fibonacci();
+            Console.WriteLine(fibo.Get(1));
+            Console.WriteLine(fibo.Get(2));
+            Console.WriteLine(fibo.Get(3));
+            Console.WriteLine(fibo.Get(4));
+            Console.WriteLine(fibo.Get(5));
+
+        }
+    }
+}
